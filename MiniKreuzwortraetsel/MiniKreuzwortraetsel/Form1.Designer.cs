10287eefb61929a072167c7bd3dbba10c07faba0
Kryptojude <68050323+Kryptojude@users.noreply.github.com>
2020-10-12 13:41:50 +0200

    initial

---
+++ MiniKreuzwortraetsel/MiniKreuzwortraetsel/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace MiniKreuzwortraetsel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GenerateCrosswordBTN = new System.Windows.Forms.Button();
+            this.baseWordTB = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // GenerateCrosswordBTN
+            // 
+            this.GenerateCrosswordBTN.Location = new System.Drawing.Point(364, 255);
+            this.GenerateCrosswordBTN.Name = "GenerateCrosswordBTN";
+            this.GenerateCrosswordBTN.Size = new System.Drawing.Size(90, 40);
+            this.GenerateCrosswordBTN.TabIndex = 0;
+            this.GenerateCrosswordBTN.Text = "Kreuzworträtsel machen!";
+            this.GenerateCrosswordBTN.UseVisualStyleBackColor = true;
+            this.GenerateCrosswordBTN.Click += new System.EventHandler(this.GenerateCrossword);
+            // 
+            // baseWordTB
+            // 
+            this.baseWordTB.Location = new System.Drawing.Point(354, 209);
+            this.baseWordTB.Name = "baseWordTB";
+            this.baseWordTB.Size = new System.Drawing.Size(100, 20);
+            this.baseWordTB.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(370, 180);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Lösungswort:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.baseWordTB);
+            this.Controls.Add(this.GenerateCrosswordBTN);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button GenerateCrosswordBTN;
+        private System.Windows.Forms.TextBox baseWordTB;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ MiniKreuzwortraetsel/MiniKreuzwortraetsel/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace MiniKreuzwortraetsel
+{
+    public partial class Form1 : Form
+    {
+        string[,] grid;
+        Point gridOrigin = new Point();
+        List<(string, string)> database = new List<(string, string)>();
+        Random random = new Random();
+        int questionCounter = 0;
+        int ts = 30;
+        List<int> xCoords = new List<int>();
+        List<int> yCoords = new List<int>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            Paint += Draw;
+        }
+
+        private void GenerateCrossword(object sender, EventArgs e)
+        {
+            string baseWord = baseWordTB.Text.ToUpper();
+
+            // Fetch and scramble database
+            FetchDatabase();
+            ScrambleDatabase();
+
+            // Find longest word
+            int longestWord = 0;
+            for (int i = 0; i < database.Count; i++)
+            {
+                (string Question, string Answer) tuple = database[i];
+                if (tuple.Answer.Length > longestWord)
+                {
+                    longestWord = tuple.Answer.Length;
+                }
+            }
+
+            // Check if baseWord is longer than longest word in db
+            if (baseWord.Length > longestWord)
+                longestWord = baseWord.Length;
+
+            // Initialize grid based on longest word
+            int maximumGridWidth = longestWord * 2;
+            grid = new string[longestWord + 1, maximumGridWidth];
+
+            // Put the base word vertically
+            Point baseQuestionTilePos = new Point(longestWord, 0);
+            FillAnswer(baseQuestionTilePos, new Point(0, 1), ("", baseWord));
+
+            // Try crossing each letter of the base word
+            for (int i = 0; i < baseWord.Length; i++)
+            {
+                CrossBaseWord(i, baseWord[i], baseQuestionTilePos);
+            }
+
+            // Shrink the grid to minimum by moving it to the top-left corner            
+            gridOrigin.X = -xCoords.Min() * ts;
+            gridOrigin.Y = -yCoords.Min() * ts;
+            xCoords.Clear();
+            yCoords.Clear();
+
+            questionCounter = 0;
+
+            Refresh();
+        }
+        private void CrossBaseWord(int baseLetterIndex, char matchLetter, Point baseQuestionTilePos)
+        {
+            // Go through database till word is found with matching letter
+            bool wordFound = false;
+            int databaseIndex = 0;
+            (string Question, string Answer) tuple = ("", "");
+            int matchIndex = 0;
+            while (!wordFound && databaseIndex < database.Count)
+            {
+                tuple = database[databaseIndex];
+                matchIndex = tuple.Answer.IndexOf(matchLetter);
+                if (matchIndex != -1)
+                    wordFound = true;
+
+                databaseIndex++;
+            }
+            if (wordFound)
+            {
+                Point newQuestionTilePos = new Point {
+                    X = baseQuestionTilePos.X - 1 - matchIndex,
+                    Y = baseQuestionTilePos.Y + baseLetterIndex + 1 };
+
+                FillAnswer(newQuestionTilePos, new Point(1, 0), tuple);
+            }
+        }
+        private void FillAnswer(Point questionTilePos, Point direction, (string Question, string Answer) tuple)
+        {
+            // Fill the question indicator into the tile
+            string arrow = (direction.X == 1) ? "►" : "\n▼";
+            grid[questionTilePos.Y, questionTilePos.X] = (questionCounter + 1) + arrow;
+            xCoords.Add(questionTilePos.X);
+            yCoords.Add(questionTilePos.Y);
+
+            int letterX = 0; // Absolute position of the current letter
+            int letterY = 0;
+            // Fill the answer into the grid letter by letter
+            for (int c = 0; c < tuple.Answer.Length; c++)
+            {
+                letterX = questionTilePos.X + (direction.X * (c + 1));
+                letterY = questionTilePos.Y + (direction.Y * (c + 1));
+                grid[letterY, letterX] = tuple.Answer[c].ToString();
+                // Save the letter position for easy shrinking of the grid later
+                xCoords.Add(letterX);
+                yCoords.Add(letterY);
+            }
+            database.Remove(tuple);
+            questionCounter++;
+        }
+        private void FetchDatabase()
+        {
+            StreamReader reader = new StreamReader("database.txt");
+            string line = reader.ReadLine();
+            while (line != null)
+            {
+                string question = line.Substring(0, line.IndexOf(';'));
+                string answer = line.Substring(line.IndexOf(';') + 1);
+                database.Add((question, answer));
+                line = reader.ReadLine();
+            }
+        }
+        private void ScrambleDatabase()
+        {
+            List<(string Question, string Answer)> database2 = new List<(string Question, string Answer)>();
+            for (int i = 0; i < database.Count; i++)
+            {
+                database2.Add(("", ""));
+            }
+
+            for (int i = 0; i < database.Count; i++)
+            {
+                while (true)
+                {
+                    int randomSpot = random.Next(database2.Count);
+                    if (database2[randomSpot].Answer == "")
+                    {
+                        database2[randomSpot] = database[i];
+                        break;
+                    }
+                }
+            }
+
+            database = database2;
+        }
+        private void Draw(object sender, PaintEventArgs e)
+        {
+            e.Graphics.TranslateTransform(gridOrigin.X, gridOrigin.Y);
+            if (grid != null)
+                for (int y = 0; y < grid.GetLength(0); y++)
+                {
+                    for (int x = 0; x < grid.GetLength(1); x++)
+                    {
+                        if (grid[y, x] != null)
+                        {
+                            Size textSize = TextRenderer.MeasureText(grid[y, x], Font);
+                            if (grid[y, x].Contains("►") ||
+                                grid[y, x].Contains("▼"))
+                            { // question tile
+                                e.Graphics.DrawRectangle(Pens.Black, x * ts, y * ts, ts, ts);
+                                e.Graphics.DrawString(grid[y, x], Font, Brushes.Red, x * ts + ts / 2 - textSize.Width / 2, y * ts + ts / 2 - textSize.Height / 2);
+                            }
+                            else if (grid[y, x] == "blocked")
+                            { // blocked tile
+                                e.Graphics.FillRectangle(Brushes.Black, x * ts, y * ts, ts, ts);
+                            }
+                            else
+                            { // letter tile
+                                e.Graphics.DrawRectangle(Pens.Black, x * ts, y * ts, ts, ts);
+                                e.Graphics.DrawString(grid[y, x], Font, Brushes.DarkBlue, x * ts + ts / 2 - textSize.Width / 2, y * ts + ts / 2 - textSize.Height / 2);
+                            }
+                        }
+                    }
+                }
+        }
+    }
+}
